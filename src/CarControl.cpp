2daa0e5d11793dc8c97be5aa9902095bf48fe7a8
Clipping <109019791+clippinglee@users.noreply.github.com>
2025-01-03 13:25:34 +0800

    Add files via upload

---
+++ src/CarControl.cpp
@@ -0,0 +1,45 @@
+#include "L298N.h"
+#include "CarControl.hpp"
+
+void smartcarContol::car_turnRight(void)
+{
+    L298N_MotorControl(0,MOTOR_FORWARD);
+    L298N_MotorControl(1,MOTOR_STOP);
+}
+
+void smartcarContol::car_turnLeft(void)
+{
+    L298N_MotorControl(0,MOTOR_STOP);
+    L298N_MotorControl(1,MOTOR_FORWARD);
+}
+
+void smartcarContol::car_forward(void)
+{
+    L298N_MotorControl(0,MOTOR_FORWARD);
+    L298N_MotorControl(1,MOTOR_FORWARD);
+}
+
+void smartcarContol::car_sotp(void)
+{
+    L298N_MotorControl(0,MOTOR_STOP);
+    L298N_MotorControl(1,MOTOR_STOP);
+}
+
+
+void smartcarContol::car_reverse(void)
+{
+    L298N_MotorControl(0,MOTOR_REVERSE);
+    L298N_MotorControl(1,MOTOR_REVERSE);
+}
+
+void smartcarContol::car_turnLeftCycle(void)
+{
+    L298N_MotorControl(0,MOTOR_REVERSE);
+    L298N_MotorControl(1,MOTOR_FORWARD);
+}
+
+void smartcarContol::car_turnRightCycle(void)
+{
+    L298N_MotorControl(0,MOTOR_FORWARD);
+    L298N_MotorControl(1,MOTOR_REVERSE);
+}
---
+++ src/CarControl.hpp
@@ -0,0 +1,13 @@
+
+
+
+class smartcarContol{
+    public:
+    void car_sotp(void);
+    void car_forward(void);
+    void car_turnRight(void);
+    void car_turnLeft(void);
+    void car_reverse(void);
+    void car_turnLeftCycle(void);
+    void car_turnRightCycle(void);
+};
